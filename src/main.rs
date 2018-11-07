a4ca410cc05cb75791e47957c67eb8b5b8e67998
Griffone <thegriffones@gmail.com>
2018-11-07 11:08:36 +0100

    Started learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate rand;
+
+use rand::Rng;
+
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+	let min = 0;
+	let max = 100;
+
+	let secret_number = rand::thread_rng().gen_range(min + 1, max + 1);
+
+	println!("Guess the number between {} and {}!", min, max);
+
+	loop {
+		// Variables are immutable by default
+		let mut guess = String::new();
+
+		// References are also immutable by default
+		io::stdin().read_line(&mut guess)
+			.expect("Failed to read line");
+
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(err) => {
+				println!("{}", err);
+				continue;
+			}
+		};
+
+		println!("You guessed: {}", guess);
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => {
+				println!("You win!");
+				break;
+			}
+		}
+	}
 }
