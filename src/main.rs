a95537bca61de819ad01a3596e3bc73c14083296
Griffone <thegriffones@gmail.com>
2018-11-08 10:40:02 +0100

    Improved syntax
    Made code more Rust consistent.
    Added shadowing comment.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use rand::Rng;
 use std::io;
 use std::cmp::Ordering;
 
+const GUESS_RANGE_BOTTOM: u32	= 0;
+const GUESS_RANGE_TOP: u32		= 100;
+
 fn main() {
-	let min = 0;
-	let max = 100;
+	// gen_range generates within [min, max) half-interval, so we add one to make it more user-friendly between min and max
+	let secret_number = rand::thread_rng().gen_range(GUESS_RANGE_BOTTOM, GUESS_RANGE_TOP) + 1;
 
-	let secret_number = rand::thread_rng().gen_range(min + 1, max + 1);
-
-	println!("Guess the number between {} and {}!", min, max);
+	println!("Guess the number between {} and {}!", GUESS_RANGE_BOTTOM, GUESS_RANGE_TOP);
 
 	loop {
 		// Variables are immutable by default
@@ -21,6 +22,7 @@ fn main() {
 		io::stdin().read_line(&mut guess)
 			.expect("Failed to read line");
 
+		// Shadow the guess variable
 		let guess: u32 = match guess.trim().parse() {
 			Ok(num) => num,
 			Err(err) => {
